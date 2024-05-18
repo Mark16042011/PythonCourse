d13676083483f7609231b3fc600e1a31d76bbfaa
kosne1 <akolia2017@list.ru>
2024-05-18 21:09:21 +0300

    feat: can delete and get users names

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.post("/names")
 def new_name(name: str):
-    file = open("names", "a")
+    file = open("names.txt", "a")
     file.write(name + "\n")
     file.close()
     return "ok"
@@ -13,15 +13,26 @@ def new_name(name: str):
 
 @app.delete("/names")
 def delete_name(name: str):
-    file = open("names", "r")
+    file = open("names.txt", "r")
+    new_names = []
     for item in file:
         if item != name:
-            file = open("names", "a")
-            file.write(item)
-    return file
+            new_names.append(item)
+    file.close()
 
-#
-# @app.get("/names")
-# def get_names():
-#     list_of_names = open("/", "r")
-#     return list_of_names
+    file = open("names.txt", "w")
+    for item in new_names:
+        file.write(item)
+    file.close()
+
+    return new_names
+
+
+@app.get("/names")
+def get_names():
+    file = open("names.txt", "r")
+    names = []
+    for item in file:
+        names.append(item)
+    file.close()
+    return names
