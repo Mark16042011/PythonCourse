bbf75413976543858682133ac8f2429208c423fa
Mark16042011 <markilnizkii@gmail.com>
2024-05-18 20:54:13 +0300

    cacaia to fignia

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-from PostSchemas import PostSchema, Comment
+from fastapi import FastAPI
 
-bad_words = ["дурак", "лрлрлрлрлр"]
-
-
-class Post:
-    def __init__(self, post: PostSchema):
-        self.user = post.user
-        self.date = post.date
-        self.likes = post.likes
-        self.text = post.text
-        self.comments = post.comments
-        for badWord in bad_words:
-            gratings = "#" * len(badWord)
-            self.text = self.text.replace(badWord, gratings)
-
-    def like(self, name: str):
-        if name not in self.likes:
-            self.likes.append(name)
-        else:
-            self.likes.remove(name)
-        return len(self.likes)
-
-    def writeComment(self, comment: Comment):
-        for item in self.comments:
-            for name in item:
-                if name == comment.user:
-                    item[name].append(comment.text)
-        return self.comments
+app = FastAPI()
 
 
-post1 = Post(PostSchema(user="oleg", date="20.04.88", likes=["user"], text="Ты дурак, лрлрлрлрлр",
-                        comments=[{"name1": ["коммент1", "коммент2"]}, {"name2": ["комент1", "коммент2"]}]))
-print(post1.like("user"))
-print(post1.writeComment(Comment(user='user1', content='213')))
+@app.post("/names")
+def new_name(name: str):
+    file = open("names", "a")
+    file.write(name + "\n")
+    file.close()
+    return "ok"
+
+
+@app.delete("/names")
+def delete_name(name: str):
+    file = open("names", "r")
+    for item in file:
+        if item != name:
+            file = open("names", "a")
+            file.write(item)
+    return file
+
+#
+# @app.get("/names")
+# def get_names():
+#     list_of_names = open("/", "r")
+#     return list_of_names
