1602f064cb665a10b9ae4ba294eeda13e490cbaa
Mark16042011 <markilnizkii@gmail.com>
2024-10-19 12:58:18 +0300

    kakish polnii etot vash git

--- main.py
+++ main.py
@@ -17,9 +17,18 @@ def new_name(name: str, date_of_birth: str):
         if temp_name == name:
             # Если пользователь с таким именем уже существует, то прокидываем ошибку
             raise HTTPException(status_code=418, detail="User with this name already exist")
-    # TODO: add id
+
+
+    biggest_id = 0
+    for user in users:
+        id = user.get("id")
+        if id > biggest_id:
+            biggest_id = id
+    biggest_id = biggest_id + 1
+
     new_user = {
         "name": name,
+        'id': biggest_id,
         "date_of_birth": date_of_birth
     }
     users.append(new_user)
@@ -36,26 +45,32 @@ def new_name(name: str, date_of_birth: str):
 
 @app.delete("/names")
 def delete_name(name: str):
-    file = open("names.txt", "r")
-    new_names = []
-    for item in file:
-        if item.replace('\n', '') != name:
-            new_names.append(item)
+    file = open("data.json", "r")
+    data: dict = json.load(file)
+    new_users = []
+    users: list[dict] = data.get("users",[])
+    for user in users:
+        if user.get("name") != name:
+            new_users.append(user)
+    print(new_users)
+
     file.close()
 
-    file = open("names.txt", "w")
-    for item in new_names:
-        file.write(item)
+
+    new_data = {
+        "users": new_users
+    }
+    file = open("data.json", "w")
+    file.write(json.dumps(new_data, indent=2))
     file.close()
 
-    return new_names
+    return new_users
 
 
 @app.get("/names")
 def get_names():
-    file = open("names.txt", "r")
-    names = []
-    for item in file:
-        names.append(item)
-    file.close()
-    return names
+    file = open("data.json", "r")
+    data: dict = json.load(file)
+    users = data.get("users")
+
+    return users
